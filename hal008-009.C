11aa12519c0b943a566ce3cbd215876ebfd5a4cb
RATDOTL <ratdotl2357@gmail.com>
2023-05-15 16:04:16 +0900

    Add output

--- hal008-009.C
+++ hal008-009.C
@@ -4,19 +4,19 @@
 
     // data_hal
     TFile *f0 = TFile :: Open("C:/Users/posei/Desktop/tokken/winscp/data_test/data_hal/hal009.root");
-    // TFile *f1 = TFile :: Open("C:/Users/posei/Desktop/tokken/winscp/data_test/data_hal/hal008.root");
+    TFile *f1 = TFile :: Open("C:/Users/posei/Desktop/tokken/winscp/data_test/data_hal/hal008.root");
     
     
     f0->cd();
     // T->Draw("a[5]>>h0(100,0,3000)");
 
-    // T->Draw("a[0]>>h0(100,1300,2000)","500<t[0]&&t[0]<650");
+    // T->Draw("a[0]>>h0(100,1100,2000)","500<t[0]&&t[0]<650");
 
-    // T->Draw("a[1]>>h0(100,1300,3000)","300<t[1]&&t[1]<700");
+    // T->Draw("a[1]>>h0(100,1000,2500)","300<t[1]&&t[1]<700");
 
-    // T->Draw("a[4]>>h0(100,1300,3000)","300<t[2]&&t[2]<800");
+    // T->Draw("a[4]>>h0(100,1000,2500)","300<t[2]&&t[2]<800");
 
-    // T->Draw("a[5]>>h0(100,1400,3000)","400<t[3]&&t[3]<700");
+    T->Draw("a[5]>>h0(100,1000,3000)","400<t[3]&&t[3]<700");
 
     // T->Draw("t[0]>>h0(100,500,650)");
     // T->Draw("t[1]>>h0(300,0,700)");
@@ -24,25 +24,28 @@
     // T->Draw("t[3]>>h0(400,0,700)");
 
     // 角度分布
-    T->Draw("a[1]/a[0]:t[1]-t[0]>>h0(2000,-500,500)","500<t[0]&&t[0]<650&&300<t[1]&&t[1]<700");
+    // T->Draw("a[1]/a[0]:t[1]-t[0]>>h0(2000,-500,500)","500<t[0]&&t[0]<650&&300<t[1]&&t[1]<700");
     // T->Draw("a[1]/a[0]:t[1]-t[0]>>h0(100,-500,500)","500<t[0]&&t[0]<650");
+
+
+
     h0->SetLineColor(2);
     h0->Draw();
-    // h0->Fit("gaus");
 
-    // f1->cd();
+    f1->cd();
     // // // T->Draw("a[0]>>h1(100,0,3000)","","same");
 
-    // T->Draw("a[0]>>h1(100,1000,1500)","!(500<t[0]&&t[0]<650)","same");
-    // // // T->Draw("a[1]>>h1(100,1200,1500)","900<t[1]&&t[1]<1500","same"); 
-    // // // T->Draw("a[1]>>h1(100,1200,1500)","","same"); //021
+    // T->Draw("a[0]>>h1(100,1100,2000)","","same");
+    // T->Draw("a[1]>>h1(100,1000,2500)","","same"); 
+    // T->Draw("a[4]>>h1(100,1000,2500)","","same"); 
+    T->Draw("a[5]>>h1(100,1000,2500)","","same"); 
 
 
 
-    // h1->SetLineColor(4);
-    // h1->Draw("same");
+    h1->SetLineColor(4);
+    h1->Draw("same");
 
 
     // c1->Print("/mnt/c/Users/posei/Desktop/tokken/winscp/data_test/output/run013_015.pdf");
-    // c1->Print("C:/Users/posei/Desktop/tokken/winscp/data_test/output/hal003_005_a[0].pdf");
+    // c1->Print("C:/Users/posei/Desktop/tokken/winscp/data_test/output/hal008_009_a[3].pdf");
 }
